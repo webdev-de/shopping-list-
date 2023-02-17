12ea9bf0000c06c4acd1a8e3ea5c69f81bef04d2
Jan Behrens <mister.webdev.de@gmail.com>
2023-02-17 23:37:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+function App() {
+
+  return (
+    <div>
+      <h1>H1 - Shopping List</h1>
+      <p>
+        Dasi ist ein <a href="">Link</a> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Blanditiis sunt numquam nesciunt exercitationem, doloremque voluptates quibusdam fuga
+        dolor animi sequi veniam debitis iusto non libero repellat asperiores culpa fugiat
+        molestias at molestiae nihil et eligendi explicabo. Eos incidunt, et alias porro quod
+        placeat est temporibus. Facilis veritatis repudiandae quibusdam nam soluta! Quis sapiente
+        rem illo quidem id repudiandae? Aperiam in totam perspiciatis quod necessitatibus, repellat amet,
+        vitae quo nesciunt ipsam atque minus provident ea sint ab, voluptatum similique aut omnis dolore.
+        Labore suscipit pariatur sapiente eius neque porro veritatis ea ipsam reiciendis ad molestiae repellendus
+        voluptatum fugiat, ex modi assumenda alias vitae velit enim eaque eum iusto commodi cupiditate molestias.
+        Culpa laudantium commodi quaerat qui sunt omnis possimus laboriosam. Fuga impedit sequi eius iure, commodi
+        debitis possimus rerum ipsum. Sint mollitia, nemo veniam nostrum alias architecto facilis deserunt at dolorem
+        minus fugiat aperiam aliquid distinctio ea quam nihil asperiores minima? Laudantium quas atque molestias est
+        quidem id molestiae dolore rerum.
+      </p>
+      <h2>H2 - Shopping List</h2>
+      <ul>
+        <li>Eintrag 1</li>
+        <li>Eintrag 2</li>
+        <li>Eintrag 3</li>
+        <li>Eintrag 4</li>
+      </ul>
+
+      <ol>
+        <li>Eintrag 1</li>
+        <li>Eintrag 2</li>
+        <li>Eintrag 3</li>
+        <li>Eintrag 4</li>
+      </ol>
+      <h3>H3 - Shopping List</h3>
+      <form>
+        <label>
+          Vorname:
+          <input type="text" name="vorname" />
+        </label>
+        <label>
+          Nachname:
+          <input type="text" name="nachname" />
+        </label>
+        <label>
+          E-Mail:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Passwort:
+          <input type="password" name="passwort" />
+        </label>
+        <button type="submit">Absenden</button>
+      </form>
+      <h4>H4 - Shopping List</h4>
+      <h5>H5 - Shopping List</h5>
+      <h6>H6 - Shopping List</h6>
+    </div>
+  )
+}
+
+export default App
