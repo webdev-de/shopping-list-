d17bc9d20a25d1005185cc326788a7e8b3e3669c
Jan Behrens <mister.webdev.de@gmail.com>
2023-02-20 22:32:53 +0100

    generated a uniqe ID

---
+++ src/assets/scss/helper/uniqueID.jsx
@@ -0,0 +1,15 @@
+const uniqueID = () => {
+
+    const timeStamp = new Date().getTime();
+
+    const hash =  'xxxx'.replace(/[xy]/g, c => {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+
+
+    return hash+(timeStamp.toString(16).substring(2));
+}
+
+export default uniqueID;
