1f94f2df49aabc52ff132c30bef2592165ba6a31
Jan Behrens <mister.webdev.de@gmail.com>
2023-02-21 23:32:09 +0100

    added List elements

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import ShoppingList_Item from "./ShoppingList_Item";
-import { FaTrashAlt, FaListAlt, FaPlus, FaSearch } from 'react-icons/fa';
+import { FaTrashAlt, FaListAlt, FaPlus, FaSearch, FaCheckCircle, FaCheck } from 'react-icons/fa';
 import uniqueID from "../../assets/scss/helper/uniqueID";
+
+const demoLists = [
+  {
+    id: "5bae675f92b3e",
+    lastupdate: 1677016588793,
+    name: "Allgemein",
+    shared: false,
+    default: true
+  }
+]
 
 const demoItems = [
   {
-    id: 1,
-    list: "demo",
-    done: true,
-    name: "Produkt 1"
-  },
-  {
-    id: 2,
-    list: "default",
-    done: false,
-    name: "Produkt 2"
-  },
-  {
-    id: 3,
-    list: "memo",
+    id: "49b4675f41392",
+    lastupdate: 1677016135205,
+    list: "5bae675f92b3e",
     done: true,
     name: "Produkt 1"
   }
 ]
 
 
+
+
 const ShoppingList = () => {
 
   const [items, setItems] = useState(demoItems);
+  const [lists, setLists] = useState(demoLists);
+
   const [itemInput, setItemInput] = useState("");
   const [currentList, setCurrentList] = useState("default")
 
@@ -45,6 +48,13 @@ const ShoppingList = () => {
     return allItems;
   }
 
+
+  const getLists = () => {
+    const allLists = lists;
+
+    return allLists;
+  }
+
   const addItem = () => {
     const newItem = {
       id: uniqueID(),
@@ -55,19 +65,33 @@ const ShoppingList = () => {
     setItems([...items, newItem])
   }
 
-
-
-
   return (
     <div>
       <h2>Ich bin die Liste</h2>
 
-      {getItems(null, currentList).length >= 1 ? getItems(null, currentList).map(item => (
-        <ShoppingList_Item
-          key={item.id} id={item.id}
-          done={item.done} name={item.name}
-        />
-      )) : "Leider kein Eintrag gefunden"}
+      <h3>Lists</h3>
+      <ul className="main-lists">
+        {getLists().length > 0 && getLists().map(list => (
+          <li key={list.id} className="item-lists">
+            {list.default && <FaCheck className="check"/>}
+            <span>{list.name}</span>
+            <span>{getItems(null, list.id).length}</span>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Items</h3>
+      <div>{new Date().getTime()}</div>
+
+      {
+        getItems(null, currentList).length >= 1 ? getItems(null, currentList).map(item => (
+          <ShoppingList_Item
+            key={item.id} id={item.id}
+            done={item.done} name={item.name}
+          />
+        )) :
+          <div>Leider wurde kein Eintrag gefunden, schade Mamelade!</div>
+      }
 
 
 
